f4f6b7ed04619facfb154dcfaa410ada90474ec8
Pablo Gonzalez <pab.gonzalez3798Qgmail.com>
2024-03-17 23:34:13 -0300

    se creo la funcion para crear recetas

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -1,19 +1,21 @@
+import Receta from "../database/model/receta.js";
+
 export const listarRecetas = (req, res)=>{
     console.log('desde listar recetas');
-    res.send('enviar lista de recetass...')
+    res.send('enviar lista de recetas...')
 }
 
-export const crearTarea = async (req, res) => {
+export const crearReceta = async (req, res) => {
     try {
-      const tareaNueva = new Tarea(req.body);
-      await tareaNueva.save();
+      const recetaNueva = new Receta(req.body);
+      await recetaNueva.save();
       res.status(201).json({
-        mensaje: "La tarea fue creada correctamente",
+        mensaje: "La receta fue creada correctamente",
       });
     } catch (error) {
       console.log(error);
       res.status(400).json({
-        mensaje: "No se pudo procesar la solicitud de crear la tarea",
+        mensaje: "No se pudo procesar la solicitud de crear la receta",
       });
     }
   };
