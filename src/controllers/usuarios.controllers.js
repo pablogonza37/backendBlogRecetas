2bdbce3ec4f03b8eb6da7510d29e0f7efd2fe661
Pablo Gonzalez <pab.gonzalez3798Qgmail.com>
2024-03-18 21:51:39 -0300

    se agregaron las funciones para listar usuarios y de obtener usuario selecionado

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -15,4 +15,29 @@ export const crearUsuario = async (req, res) => {
     }
   };
 
+  export const listarUsuarios = async (req, res) => {
+    try {
+      const usuarios = await Usuario.find();
+      res.status(200).json(usuarios);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({
+        mensaje: "No se pudo encontrar la lista de usuarios",
+      });
+    }
+  };
+
+  export const obtenerUsuario = async (req, res) => {
+    try {
+      const usuarioBuscado = await Usuario.findById(req.params.id);
+      res.status(200).json(usuarioBuscado);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({
+        mensaje: "No se pudo encontrar el usuario",
+      });
+    }
+  };
+  
+
   
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { crearUsuario } from "../controllers/usuarios.controllers.js";
+import { crearUsuario, listarUsuarios, obtenerUsuario } from "../controllers/usuarios.controllers.js";
 
 const router = Router();
 
-router.route("/usuarios").post(crearUsuario);
+router.route("/usuarios").post(crearUsuario).get(listarUsuarios);
+router.route("/usuarios/:id").get(obtenerUsuario)
 
 export default router;
