8aa54cd6372e245db69fe6266b098bebfeeb73ef
Pablo Gonzalez <pab.gonzalez3798Qgmail.com>
2024-03-18 22:13:08 -0300

    se agrego la logica para editar un usuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,61 +1,76 @@
 import Usuario from "../database/model/usuario.js";
 
 export const crearUsuario = async (req, res) => {
-    try {
-      const usuarioNuevo = new Usuario(req.body);
-      await usuarioNuevo.save();
-      res.status(201).json({
-        mensaje: "Usuario registrado correctamente",
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({
-        mensaje: "No se pudo procesar la solicitud de crear usuario",
+  try {
+    const usuarioNuevo = new Usuario(req.body);
+    await usuarioNuevo.save();
+    res.status(201).json({
+      mensaje: "Usuario registrado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "No se pudo procesar la solicitud de crear usuario",
+    });
+  }
+};
+
+export const listarUsuarios = async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "No se pudo encontrar la lista de usuarios",
+    });
+  }
+};
+
+export const obtenerUsuario = async (req, res) => {
+  try {
+    const usuarioBuscado = await Usuario.findById(req.params.id);
+    res.status(200).json(usuarioBuscado);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "No se pudo encontrar el usuario",
+    });
+  }
+};
+
+export const borrarUsuario = async (req, res) => {
+  try {
+    const usuarioBuscado = await Usuario.findById(req.params.id);
+    if (!usuarioBuscado) {
+      return res.status(404).json({
+        mensaje: "No se pudo eliminar el usuario, el id es incorrecto",
       });
     }
-  };
+    await Usuario.findByIdAndDelete(req.params.id);
+    res.status(200).json({ mensaje: "El usuario fue eliminado correctamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      mensaje: "ocurrio un error al intentar eliminar el usuario",
+    });
+  }
+};
 
-  export const listarUsuarios = async (req, res) => {
-    try {
-      const usuarios = await Usuario.find();
-      res.status(200).json(usuarios);
-    } catch (error) {
-      console.log(error);
-      res.status(404).json({
-        mensaje: "No se pudo encontrar la lista de usuarios",
+export const editarUsuario = async (req, res) => {
+  try {
+    const usuarioBuscado = await Usuario.findById(req.params.id);
+    if (!usuarioBuscado) {
+      return res.status(404).json({
+        mensaje: "No se pudo editar el usuario, el id es incorrecto",
       });
     }
-  };
-
-  export const obtenerUsuario = async (req, res) => {
-    try {
-      const usuarioBuscado = await Usuario.findById(req.params.id);
-      res.status(200).json(usuarioBuscado);
-    } catch (error) {
-      console.log(error);
-      res.status(404).json({
-        mensaje: "No se pudo encontrar el usuario",
-      });
-    }
-  };
-
-  export const borrarUsuario = async (req, res) => {
-    try {
-      const usuarioBuscado = await Usuario.findById(req.params.id);
-      if (!usuarioBuscado) {
-        return res.status(404).json({
-          mensaje: "No se pudo eliminar el usuario, el id es incorrecto",
-        });
-      }
-      await Usuario.findByIdAndDelete(req.params.id);
-      res.status(200).json({ mensaje: "El usuario fue eliminado correctamente" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        mensaje: "ocurrio un error al intentar eliminar el usuario",
-      });
-    }
-  };
-  
-
-  
+    await Usuario.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ mensaje: "El usuario fue modificado exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      mensaje: "ocurrio un error al intentar editar el usuario",
+    });
+  }
+};
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
-import { borrarUsuario, crearUsuario, listarUsuarios, obtenerUsuario } from "../controllers/usuarios.controllers.js";
+import {
+  borrarUsuario,
+  crearUsuario,
+  editarUsuario,
+  listarUsuarios,
+  obtenerUsuario,
+} from "../controllers/usuarios.controllers.js";
 
 const router = Router();
 
 router.route("/usuarios").post(crearUsuario).get(listarUsuarios);
-router.route("/usuarios/:id").get(obtenerUsuario).delete(borrarUsuario)
+router
+  .route("/usuarios/:id")
+  .get(obtenerUsuario)
+  .delete(borrarUsuario)
+  .put(editarUsuario);
 
 export default router;
