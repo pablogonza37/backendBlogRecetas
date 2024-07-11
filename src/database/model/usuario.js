7c465bb2218c1d109b93f82d0b625497e5bd8de6
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-07-11 13:01:36 -0300

    modificacion schema usuario

--- src/database/model/usuario.js
+++ src/database/model/usuario.js
@@ -3,26 +3,26 @@ import mongoose from "mongoose";
 const usuarioSchema = new mongoose.Schema({
     nombreUsuario: {
         type: String,
-        required: true,
-        minLength: 3,
-        maxLength: 50
-    },
-    email: {
+        required: [true, "El nombre es obligatorio"],
+        minLength: [2, "Debe ingresar como mínimo 2 caracteres para el nombre de usuario."],
+        maxLength: [50, "Debe ingresar como máximo 50 caracteres para el nombre de usuario."]
+      },
+      email: {
         type: String,
-        required: true,
-        unique: true,
-        match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    },
-    rol: {
+        required: [true, "El email es obligatorio"],
+        match: [/^\S+@\S+$/i, "El email ingresado no es válido"]
+      },
+      rol: {
         type: String,
         required: true,
         enum: ['usuario', 'admin']
     },
     password: {
         type: String,
-        required: true,
-        minLength: 6
-    }
+        required: [true, "La contraseña es obligatoria"],
+        minLength: [6, "La contraseña debe tener al menos 6 caracteres"]
+      },
+      
 });
 
 const Usuario = mongoose.model('Usuario', usuarioSchema);
